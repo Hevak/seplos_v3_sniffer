afccc9a589cf5e1208e6e092a97f1c70955c76b4
DpunktS <56207383+DpunktS@users.noreply.github.com>
2024-11-25 07:50:01 +0100

    Update seplos_parser.cpp

--- esphome/components/seplos_parser/seplos_parser.cpp
+++ esphome/components/seplos_parser/seplos_parser.cpp
@@ -6,13 +6,14 @@
 namespace esphome {
 namespace seplos_parser {
 
-SeplosParser::SeplosParser(UARTComponent *uart, int bms_count, int throttle_interval) : UARTDevice(uart), uart_(uart), max_bms_count(bms_count), throttle_interval(throttle_interval)  {
+SeplosParser::SeplosParser(UARTComponent *uart, int bms_count, int throttle_interval) : UARTDevice(uart), uart_(uart), max_bms_count(bms_count), throttle_interval_(throttle_interval)  {
   for (int i = 0; i < max_bms_count; i++) {
     bms.emplace_back(BMSData());
   }
 }
 
 void SeplosParser::setup() {
+  ESP_LOGI("seplos_parser", "Setup called for SeplosParser with ID");
   for (int i = 0; i < max_bms_count; i++) {
     std::string prefix = "BMS " + std::to_string(i + 1) + " ";
 
@@ -250,6 +251,7 @@ void SeplosParser::setup() {
 }
 
 void SeplosParser::loop() {
+  ESP_LOGD("seplos_parser", "Loop called for SeplosParser");
   if (!this->uart_->available()) {
     ESP_LOGW("seplos_parser", "UART not available!");
     return;
