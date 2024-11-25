7f32039450027867a89e1112dd3a34ad7a0c27b9
DpunktS <56207383+DpunktS@users.noreply.github.com>
2024-11-25 06:55:05 +0100

    Update sensor.py

--- esphome/components/seplos_parser/sensor.py
+++ esphome/components/seplos_parser/sensor.py
@@ -8,6 +8,7 @@ DEPENDENCIES = ['uart']
 seplos_parser_ns = cg.esphome_ns.namespace('seplos_parser')
 SeplosParser = seplos_parser_ns.class_('SeplosParser', cg.Component, uart.UARTDevice)
 
+# Definiere die Konfigurationsoptionen
 CONF_UART_ID = 'uart_id'
 CONF_THROTTLE_INTERVAL = 'throttle_interval'
 
@@ -15,10 +16,11 @@ CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SeplosParser),
     cv.Required(CONF_BMS_COUNT): cv.int_,
     cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-    cv.Optional(CONF_THROTTLE_INTERVAL, default=5): cv.int_,  # Standardwert 5 Sekunden
+    cv.Optional(CONF_THROTTLE_INTERVAL, default=5): cv.int_,  # Standardwert: 5 Sekunden
 }).extend(sensor.SENSOR_SCHEMA)
 
 async def to_code(config):
+    # Erstelle eine neue Instanz von SeplosParser
     var = cg.new_Pvariable(config[CONF_ID], config[CONF_UART_ID], config[CONF_BMS_COUNT], config[CONF_THROTTLE_INTERVAL])
     await cg.register_component(var, config)
-    uart.setup_uart(var.uart, config)
+    await uart.register_uart_device(var, config[CONF_UART_ID])
