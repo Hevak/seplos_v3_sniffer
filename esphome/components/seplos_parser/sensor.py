8456c1618d9a866415b21cd18c1ced8627489a87
DpunktS <56207383+DpunktS@users.noreply.github.com>
2024-11-30 19:32:33 +0100

    Update sensor.py

--- esphome/components/seplos_parser/sensor.py
+++ esphome/components/seplos_parser/sensor.py
@@ -1,4 +1,3 @@
-
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, uart
@@ -12,31 +11,28 @@ CONF_THROTTLE_INTERVAL = 'throttle_interval'
 DEPENDENCIES = ['uart']
 
 seplos_parser_ns = cg.esphome_ns.namespace('seplos_parser')
-SeplosParser = seplos_parser_ns.class_("SeplosParser", cg.Component, uart.UARTDevice)
-
-CONFIG_SCHEMA = sensor.sensor_schema(SeplosParser).extend(
-    {
-        cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-        cv.Required(CONF_BMS_COUNT): cv.int_range(min=1, max=16),
-        cv.Required(CONF_THROTTLE_INTERVAL): cv.positive_int,
-    }
+SeplosParser = seplos_parser_ns.class_(
+    "SeplosParser", cg.Component, uart.UARTDevice
 )
 
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(SeplosParser),
+    cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+    cv.Required(CONF_BMS_COUNT): cv.int_range(min=1, max=16),
+    cv.Required(CONF_THROTTLE_INTERVAL): cv.positive_int,
+})
+
+
 async def to_code(config):
-    # Erstelle die SeplosParser-Instanz
-    var = cg.new_Pvariable(
-        config[CONF_ID],
-        await cg.get_variable(config[CONF_UART_ID]),  # Hole die UART-Instanz
-        config[CONF_BMS_COUNT],
-        config[CONF_THROTTLE_INTERVAL]
-    )
-    # Registriere die SeplosParser-Komponente
-    await cg.register_component(var, config)
+    try:
+        # Erstellt die Instanz des SeplosParser
+        var = cg.new_Pvariable(
+            config[CONF_ID],
+            await cg.get_variable(config[CONF_UART_ID]),
+            config[CONF_BMS_COUNT],
+            config[CONF_THROTTLE_INTERVAL]
+        )
+        await cg.register_component(var, config)
+        await uart.register_uart_device(var, config[CONF_UART_ID])
     except Exception as e:
-        cg.esphome_ns.logger.error(f"Error while registering component: {e}")
-
-    # Registriere die UART-Instanz für die SeplosParser-Komponente
-    await uart.register_uart_device(var, config[CONF_UART_ID])
-    except Exception as e:
-        cg.esphome_ns.logger.error(f"Error while registering UART device: {e}")
-
+        cg.esphome_ns.logger.error(f"Error in SeplosParser registration: {e}")
