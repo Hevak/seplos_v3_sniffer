da95907f1273b65e5e387aa19d475e98bd64f927
DpunktS <56207383+DpunktS@users.noreply.github.com>
2024-12-01 17:03:56 +0100

    Update seplos_parser.cpp

--- esphome/components/seplos_parser/seplos_parser.cpp
+++ esphome/components/seplos_parser/seplos_parser.cpp
@@ -7,17 +7,22 @@ namespace seplos_parser {
 static const char *TAG = "seplos_parser.component";
 
 void SeplosParser::setup() {
-
+    ESP_LOGI("seplos", "Setup for %d BMS devices completed.", this->bms_count_);
 }
 
 void SeplosParser::loop() {
-
+    ESP_LOGI("seplos", "Polling BMS data...");
+    this->read_data_();
 }
 
 void SeplosParser::dump_config(){
     ESP_LOGCONFIG(TAG, "Seplos Parser");
 }
-
+void read_data_() {
+    for (int i = 0; i < this->bms_count_; i++) {
+      ESP_LOGI("seplos", "Simulating data for BMS %d", i);
+      // Hier kann die echte Lese- und Verarbeitungslogik implementiert werden.
+    }
 
 }  // namespace seplos_parser
 }  // namespace esphome
