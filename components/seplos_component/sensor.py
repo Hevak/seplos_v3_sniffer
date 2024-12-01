f54a02e9e933e2ce9c13a808bb11bf74966ee66f
DpunktS <56207383+DpunktS@users.noreply.github.com>
2024-12-01 13:58:56 +0100

    Update sensor.py

--- components/seplos_component/sensor.py
+++ components/seplos_component/sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, sensor
-from esphome.const import CONF_ID, UNIT_VOLT, UNIT_AMPERE, UNIT_PERCENT, ICON_FLASH, STATE_CLASS_MEASUREMENT
+from esphome.const import CONF_ID, CONF_UART_ID
 
 DEPENDENCIES = ["uart"]
 
@@ -9,7 +9,6 @@ seplos_ns = cg.esphome_ns.namespace("seplos_component")
 SeplosComponent = seplos_ns.class_("SeplosComponent", cg.PollingComponent, uart.UARTDevice)
 
 CONF_BMS_COUNT = "bms_count"
-CONF_UART_ID = "uart_id"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SeplosComponent),
@@ -22,3 +21,4 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID], uart_component, config[CONF_BMS_COUNT])
     yield cg.register_component(var, config)
     yield uart.register_uart_device(var, config)
+
