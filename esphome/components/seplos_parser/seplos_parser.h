f79e2b923d089252cc61f8812ad1e70bd9441a79
DpunktS <56207383+DpunktS@users.noreply.github.com>
2024-12-01 16:59:35 +0100

    Update seplos_parser.h

--- esphome/components/seplos_parser/seplos_parser.h
+++ esphome/components/seplos_parser/seplos_parser.h
@@ -1,31 +1,17 @@
 #pragma once
-#include "esphome.h"
 
-class SeplosParser : public PollingComponent, public UARTDevice {
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace seplos_parser {
+
+class SeplosParser : public Component {
  public:
-  SeplosParser(UARTComponent *parent, int bms_count, uint32_t update_interval = 1000)
-      : PollingComponent(update_interval), UARTDevice(parent) {
-    this->bms_count_ = bms_count;
-    this->sensors_.resize(bms_count);
-  }
-
-  void setup() override {
-    ESP_LOGI("seplos", "Setup for %d BMS devices completed.", this->bms_count_);
-  }
-
-  void update() override {
-    ESP_LOGI("seplos", "Polling BMS data...");
-    this->read_data_();
-  }
-
- private:
-  int bms_count_;
-  std::vector<Sensor *> sensors_;
-
-  void read_data_() {
-    for (int i = 0; i < this->bms_count_; i++) {
-      ESP_LOGI("seplos", "Simulating data for BMS %d", i);
-      // Hier kann die echte Lese- und Verarbeitungslogik implementiert werden.
-    }
-  }
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
 };
+
+
+}  // namespace seplos_parser
+}  // namespace esphome
