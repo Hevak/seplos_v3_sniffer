33a1bb873676c99e79aa3a78edb77b167f078480
DpunktS <56207383+DpunktS@users.noreply.github.com>
2024-12-01 16:56:44 +0100

    Update seplos_parser.cpp

--- esphome/components/seplos_parser/seplos_parser.cpp
+++ esphome/components/seplos_parser/seplos_parser.cpp
@@ -1,1 +1,23 @@
+#include "esphome/core/log.h"
 #include "seplos_parser.h"
+
+namespace esphome {
+namespace seplos_parser {
+
+static const char *TAG = "seplos_parser.component";
+
+void SeplosParser::setup() {
+
+}
+
+void SeplosParser::loop() {
+
+}
+
+void SeplosParser::dump_config(){
+    ESP_LOGCONFIG(TAG, "Seplos Parser");
+}
+
+
+}  // namespace seplos_parser
+}  // namespace esphome
