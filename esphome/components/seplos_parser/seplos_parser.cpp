36d1d883dd633e949c8f25ee37f1fd200d014407
DpunktS <56207383+DpunktS@users.noreply.github.com>
2024-12-03 15:08:39 +0100

    Update seplos_parser.cpp

--- esphome/components/seplos_parser/seplos_parser.cpp
+++ esphome/components/seplos_parser/seplos_parser.cpp
@@ -9,6 +9,15 @@ static const char *TAG = "seplos_parser.component";
 SeplosParser::SeplosParser(esphome::uart::UARTComponent *uart_parent, int bms_count) {
   this->uart_parent_ = uart_parent;
   this->bms_count_ = bms_count;
+
+  // Initialisierung der Sensor-Listen
+  for (int i = 0; i < bms_count; i++) {
+    this->pack_voltages.push_back(new sensor::Sensor());
+    this->currents.push_back(new sensor::Sensor());
+    this->socs.push_back(new sensor::Sensor());
+    this->sohs.push_back(new sensor::Sensor());
+    this->cycle_counts.push_back(new sensor::Sensor());
+  }
 }
 
 void SeplosParser::setup() {
@@ -16,11 +25,21 @@ void SeplosParser::setup() {
 }
 
 void SeplosParser::loop() {
-    ESP_LOGI("seplos", "Polling BMS data...");
-    for (int i = 0; i < this->bms_count_; i++) {
-      ESP_LOGI("seplos", "Simulating data for BMS %d", i);
-      // Hier kann die echte Lese- und Verarbeitungslogik implementiert werden.
-    }
+//    ESP_LOGI("seplos", "Polling BMS data...");
+  for (int i = 0; i < this->bms_count_; i++) {
+    float pack_voltage = 50.0 + i;  // Beispielwert
+    float current = 5.0 * (i + 1);  // Beispielwert
+    float soc = 80.0 - i;           // Beispielwert
+    float soh = 90.0 - (i * 2);     // Beispielwert
+    int cycle_count = 100 + i;      // Beispielwert
+
+    // Werte an die Sensoren übergeben
+    this->pack_voltages[i]->publish_state(pack_voltage);
+    this->currents[i]->publish_state(current);
+    this->socs[i]->publish_state(soc);
+    this->sohs[i]->publish_state(soh);
+    this->cycle_counts[i]->publish_state(cycle_count);
+  }
 }
 
 void SeplosParser::dump_config(){
