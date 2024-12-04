42a4d3a96bf5fe554f621a5156e7d9c65cd779e6
DpunktS <56207383+DpunktS@users.noreply.github.com>
2024-12-04 17:06:43 +0100

    Update __init__.py

--- esphome/components/seplos_parser/__init__.py
+++ esphome/components/seplos_parser/__init__.py
@@ -6,18 +6,31 @@ from esphome.const import CONF_ID, CONF_UART_ID
 DEPENDENCIES = ["uart"]
 
 seplos_parser_ns = cg.esphome_ns.namespace("seplos_parser")
-SeplosParser = seplos_parser_ns.class_("SeplosParser", cg.PollingComponent, uart.UARTDevice)
+SeplosParser = seplos_parser_ns.class_("SeplosParser", cg.Comonent) #cg.PollingComponent, uart.UARTDevice)
 
 CONF_BMS_COUNT = "bms_count"
+CONF_UART_ID = "uart_id" #neu
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SeplosParser),
     cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
     cv.Required(CONF_BMS_COUNT): cv.int_range(min=1, max=16),
-}).extend(cv.COMPONENT_SCHEMA)
+    cv.Optional(CONF_SENSORS, default=[]): cv.ensure_list(sensor.SENSOR_SCHEMA),
+}).extend(uart.UART_Device_SCHEMA) #cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    uart_component = await cg.get_variable(config[CONF_UART_ID])
-    var = cg.new_Pvariable(config[CONF_ID], uart_component, config[CONF_BMS_COUNT])
+    # Erstelle eine Instanz der SeplosParser-Komponente
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
+
+    # UART-Komponente verknüpfen
+    uart_component = await cg.get_variable(config[CONF_UART_ID])
+    cg.add(var.set_uart_parent(uart_component))
+
+    # BMS-Anzahl setzen
+    cg.add(var.set_bms_count(config[CONF_BMS_COUNT]))
+
+    # Sensoren hinzufügen
+    for sensor_config in config[CONF_SENSORS]:
+        sens = await sensor.new_sensor(sensor_config)
+        cg.add(var.add_sensor(sens))
