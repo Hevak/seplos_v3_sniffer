5b12200c63ec1567228437b8490ba7eb3b5dc4e0
DpunktS <56207383+DpunktS@users.noreply.github.com>
2024-12-06 21:40:27 +0100

    Update __init__.py

--- esphome/components/seplos_parser/__init__.py
+++ esphome/components/seplos_parser/__init__.py
@@ -7,6 +7,8 @@ DEPENDENCIES = ["uart"]
 
 MULTI_CONF = True
 
+CONF_BMS_COUNT = "bms_count"
+CONF_UART_ID = "uart_id"
 CONF_SEPLOS_PARSER_ID = "seplos_parser_id"
 
 seplos_parser_ns = cg.esphome_ns.namespace("seplos_parser")
@@ -20,12 +22,19 @@ HUB_CHILD_SCHEMA = cv.Schema(
 )
 
 CONFIG_SCHEMA = ( 
-    cv.Schema({cv.GenerateID(): cv.declare_id(SeplosParser)})
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(SeplosParser),
+        cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+        cv.Optional(CONF_BMS_COUNT, default=1): cv.int_,
+    })
         .extend(cv.COMPONENT_SCHEMA)
-        .extend(uart.UART_DEVICE_SCHEMA)
+        .extend(uart.UART_COMPONENT_SCHEMA)
 )
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
+#    await uart.register_uart_device(var, config)
+    uart_component = await cg.get_variable(config[CONF_UART_ID])
+    cg.add(var.set_uart(uart_component))
+    cg.add(var.set_bms_count(config[CONF_BMS_COUNT]))
