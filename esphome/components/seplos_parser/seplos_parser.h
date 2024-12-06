643c3695eea24f6aca4a7cb8be637e65f2bf2774
DpunktS <56207383+DpunktS@users.noreply.github.com>
2024-12-06 20:56:49 +0100

    Update seplos_parser.h

--- esphome/components/seplos_parser/seplos_parser.h
+++ esphome/components/seplos_parser/seplos_parser.h
@@ -2,6 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/defines.h"
+#include "esphome/components/uart/uart.h"
 
 #ifdef USE_SENSOR
 #include "esphome/components/sensor/sensor.h"
