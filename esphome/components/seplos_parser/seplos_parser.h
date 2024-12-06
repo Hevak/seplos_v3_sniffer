da0305a25f089625e67e847f8de55b0c860cb7d9
DpunktS <56207383+DpunktS@users.noreply.github.com>
2024-12-06 21:46:24 +0100

    Update seplos_parser.h

--- esphome/components/seplos_parser/seplos_parser.h
+++ esphome/components/seplos_parser/seplos_parser.h
@@ -42,6 +42,12 @@ class SeplosParser : public uart::UARTDevice, public Component {
   void setup() override;
   void loop() override;
   void dump_config() override;
+ public:
+  void set_bms_count(int bms_count);  // Methode deklarieren
+  // Andere bestehende Methoden und Variablen...
+
+ private:
+  int bms_count_;  // Variable zur Speicherung von bms_count
 };
 
 }  // namespace seplos_parser
