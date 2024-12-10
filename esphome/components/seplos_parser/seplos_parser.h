fc293174ccce05986bb3d5727910ca75daffaaf9
DpunktS <56207383+DpunktS@users.noreply.github.com>
2024-12-10 19:35:56 +0100

    Update seplos_parser.h

--- esphome/components/seplos_parser/seplos_parser.h
+++ esphome/components/seplos_parser/seplos_parser.h
@@ -3,6 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/core/defines.h"
 #include "esphome/components/uart/uart.h"
+#include <vector>
 
 #ifdef USE_SENSOR
 #include "esphome/components/sensor/sensor.h"
@@ -39,18 +40,21 @@ class SeplosParser : public uart::UARTDevice, public Component {
 // public:
 //  void register_text_sensor(text_sensor::TextSensor *obj) { this->text_sensors_.push_back(obj); }
 //#endif
+  void set_bms_count(int bms_count);
   void setup() override;
   void loop() override;
   void dump_config() override;
- public:
-  void set_bms_count(int bms_count);  // Methode deklarieren
+// public:
+//  void set_bms_count(int bms_count);  // Methode deklarieren
   // Andere bestehende Methoden und Variablen...
 
  private:
   int bms_count_;  // Variable zur Speicherung von bms_count
  protected:
-  sensor::Sensor *bms0_pack_voltage = nullptr;
-  sensor::Sensor *bms0_current = nullptr;
+  //sensor::Sensor *bms0_pack_voltage = nullptr;
+  //sensor::Sensor *bms0_current = nullptr;
+  std::vector<sensor::Sensor *> pack_voltage_;
+  std::vector<sensor::Sensor *> current_;
 };
 
 }  // namespace seplos_parser
