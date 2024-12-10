6862fd9a6bde6cdc88d98fdccdf348b1d9b4d050
DpunktS <56207383+DpunktS@users.noreply.github.com>
2024-12-10 19:46:21 +0100

    Update seplos_parser.cpp

--- esphome/components/seplos_parser/seplos_parser.cpp
+++ esphome/components/seplos_parser/seplos_parser.cpp
@@ -8,17 +8,33 @@ namespace seplos_parser {
 static const char *TAG = "seplos_parser.component";
 
 void SeplosParser::setup() {
+   pack_voltage_.resize(bms_count_, nullptr);
+   current_.resize(bms_count_, nullptr);
+
    for (auto *sensor : this->sensors_) {
-      if (sensor->get_name() == "bms0 pack_voltage") {
-            bms0_pack_voltage = sensor;}
-      if (sensor->get_name() == "bms0 current") {
-            bms0_current = sensor;}
-   }
+    for (int i = 0; i < bms_count_; i++) {
+      // Erstelle die erwarteten Namen für Spannung und Strom
+      std::string pack_voltage_name = "bms" + std::to_string(i) + " pack_voltage";
+      std::string current_name = "bms" + std::to_string(i) + " current";
+
+      if (sensor->get_name() == pack_voltage_name) {
+        bms[i]_pack_voltage = sensor;}
+      if (sensor->get_name() == current_name) {
+        bms[i]_current = sensor;}
+    }
+   //for (auto *sensor : this->sensors_) {
+   //   if (sensor->get_name() == "bms0 pack_voltage") {
+   //         bms0_pack_voltage = sensor;}
+   //   if (sensor->get_name() == "bms0 current") {
+   //         bms0_current = sensor;}
+   //}
 }
 
 void SeplosParser::loop() {
   bms0_pack_voltage->publish_state(5.0);
   bms0_current->publish_state(5.0);
+  bms1_pack_voltage->publish_state(5.0);
+  bms1_current->publish_state(5.0);
   //ESP_LOGI("seplos", "Polling BMS data...");
   //id(bms0_pack_voltage).publish_state(5.0);
   //this->sensors_ "bms0 pack_voltage"->publish_state(5);
