b152ffc7611e0940bdb5791cbdcdc32b3fdfe35f
DpunktS <56207383+DpunktS@users.noreply.github.com>
2024-12-13 22:35:16 +0100

    Update seplos_parser.h

--- esphome/components/seplos_parser/seplos_parser.h
+++ esphome/components/seplos_parser/seplos_parser.h
@@ -64,11 +64,11 @@ class SeplosParser : public uart::UARTDevice, public Component {
   std::vector<sensor::Sensor *> pack_voltage_;
   std::vector<sensor::Sensor *> current_;
   std::vector<sensor::Sensor *> remaining_capacity_;
-  std::vector<sensor::Sensor *> total_capacity_;
-  std::vector<sensor::Sensor *> total_discharge_capacity_;
-  std::vector<sensor::Sensor *> soc_;
-  std::vector<sensor::Sensor *> soh_;
-  std::vector<sensor::Sensor *> cycle_count_;
+  //std::vector<sensor::Sensor *> total_capacity_;
+  //std::vector<sensor::Sensor *> total_discharge_capacity_;
+  //std::vector<sensor::Sensor *> soc_;
+  //std::vector<sensor::Sensor *> soh_;
+  //std::vector<sensor::Sensor *> cycle_count_;
 };
 
 }  // namespace seplos_parser
