580089565601fdecf342628162bf95a016c529dc
DpunktS <56207383+DpunktS@users.noreply.github.com>
2024-12-14 20:30:26 +0100

    Update sensor.py

--- esphome/components/seplos_parser/sensor.py
+++ esphome/components/seplos_parser/sensor.py
@@ -1,14 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import UNIT_VOLT, UNIT_AMPERE, UNIT_PERCENT, ICON_EMPTY
+from esphome.const import UNIT_VOLT, UNIT_EMPTY, UNIT_PERCENT, ICON_EMPTY
 from . import HUB_CHILD_SCHEMA, CONF_SEPLOS_PARSER_ID
 
 DEPENDENCIES = ["seplos_parser"]
 
 CONFIG_SCHEMA = (
     sensor.sensor_schema(
-      unit_of_measurement=UNIT_VOLT, icon=ICON_EMPTY, accuracy_decimals=2
+      unit_of_measurement=UNIT_EMPTY, icon=ICON_EMPTY, accuracy_decimals=2
     )
     .extend(HUB_CHILD_SCHEMA)
     .extend(cv.COMPONENT_SCHEMA)
